ba4a64f4ab8be89b21699421a0cb3ad1c0f6060a
Kanishk10k <115786716+Kanishk10k@users.noreply.github.com>
2024-03-03 13:28:44 +0530

    Add files via upload

---
+++ a023ConditionalStatements.java
@@ -0,0 +1,25 @@
+//imp. note-  Paranthesis()
+//             curly braces {}
+
+//if-else Statements
+// if(condition){
+
+// }
+// else{
+
+// }
+
+
+
+public class a023ConditionalStatements {
+    public static void main(String[] args) {
+        int age=22;
+        if (age>=18) {
+            System.out.println("adult: drive,vote");
+        }
+        
+        else{
+            System.out.println("not adult");
+        }
+    }
+}
